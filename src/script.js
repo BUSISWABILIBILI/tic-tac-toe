37e0916c2b5130066b5a66d1f35af4ad17b78173
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-11 11:34:26 +0200

    Create gameboard module

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
+const Gameboard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
 
+  const getBoard = () => board;
+
+  const placeMark = (index, mark) => {
+    if (board[index] === "") {
+      return false;
+    }
+
+    board[index] = mark;
+    return true;
+  };
+
+  const resetBoard = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
+
+  const printBoard = () => {
+    console.log(board);
+  };
+
+  return { getBoard, placeMark, resetBoard, printBoard };
+})();
+
+Gameboard.placeMark(0, "X");
+Gameboard.placeMark(4, "O");
+Gameboard.printBoard();
